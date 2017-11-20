52ed9b9c1af4f1d892a572562028a52a79e9569e
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2017-11-20 13:43:13 +0100

    Move tests to html file to avoid scrambling

--- src/index.js
+++ src/index.js
@@ -11,23 +11,7 @@ function methodThatShouldNotBeObfuscated() {
   c2.doStuff();
 
 
-  // Test properties that should be obfuscated
-
-  if (window.chunks && window.chunks.chunk2 && window.chunks.chunk2.ChunkTwo) {
-    console.error('ChunkTwo exposed!');
-  }
-
-  if (c2.__proto__ && c2.__proto__.doStuff) {
-    console.error('ChunkTwo.doStuff exposed!');
-  }
-
-  if (c2.innerClassInstance) {
-    console.error('ChunkTwo.innerClassInstance exposed!');
-
-    if (c2.innerClassInstance.instanceFieldThatShouldBeObfuscated) {
-      console.error('ChunkTwo.innerClassInstance.instanceFieldThatShouldBeObfuscated exposed!');
-    }
-  }
+  testProperties(c2);
 }
 
 methodThatShouldNotBeObfuscated();
