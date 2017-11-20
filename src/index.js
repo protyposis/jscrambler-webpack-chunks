21fd7d4ba5e9ed2723d0fa26b9745b21b9f59e40
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2017-11-20 12:14:51 +0100

    Add exposure tests

--- src/index.js
+++ src/index.js
@@ -9,6 +9,25 @@ function methodThatShouldNotBeObfuscated() {
   const c2 = new ChunkTwo();
   console.log(c2);
   c2.doStuff();
+
+
+  // Test properties that should be obfuscated
+
+  if (window.chunks && window.chunks.chunk2 && window.chunks.chunk2.ChunkTwo) {
+    console.error('ChunkTwo exposed!');
+  }
+
+  if (c2.__proto__ && c2.__proto__.doStuff) {
+    console.error('ChunkTwo.doStuff exposed!');
+  }
+
+  if (c2.innerClassInstance) {
+    console.error('ChunkTwo.innerClassInstance exposed!');
+
+    if (c2.innerClassInstance.instanceFieldThatShouldBeObfuscated) {
+      console.error('ChunkTwo.innerClassInstance.instanceFieldThatShouldBeObfuscated exposed!');
+    }
+  }
 }
 
 methodThatShouldNotBeObfuscated();
