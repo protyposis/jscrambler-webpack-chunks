e042a562b4c21de672e11fc3650a1d63e33430a6
Mario Guggenberger <mario.guggenberger@bitmovin.com>
2017-11-20 11:27:40 +0100

    Log loaded modules

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import {ChunkOne} from './ChunkOne';
 import {ChunkTwo} from './ChunkTwo';
 
 function methodThatShouldNotBeObfuscated() {
-  new ChunkOne().doStuff();
-  new ChunkTwo().doStuff();
+  const c1 = new ChunkOne();
+  console.log(c1);
+  c1.doStuff();
+
+  const c2 = new ChunkTwo();
+  console.log(c2);
+  c2.doStuff();
 }
 
 methodThatShouldNotBeObfuscated();
